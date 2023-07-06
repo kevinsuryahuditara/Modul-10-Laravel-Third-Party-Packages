b3e8af86a7d8f6748db8164f5b63e930571b3adf
kevinsuryahuditara <huditara@gmail.com>
2023-07-06 12:45:42 +0700

    percobaan2

---
+++ resources/views/employee/index.blade.php
@@ -0,0 +1,78 @@
+@extends('layouts.app')
+
+@section('content')
+
+<div class="container mt-4">
+    <div class="row mb-0">
+        <div class="col-lg-9 col-xl-6">
+            <h4 class="mb-3">{{ $pageTitle }}</h4>
+        </div>
+        <div class="col-lg-3 col-xl-6">
+            <ul class="list-inline mb-0 float-end">
+                <li class="list-inline-item">
+                    <a href="{{ route('employees.exportExcel') }}" class="btn btn-outline-success">
+                        <i class="bi bi-download me-1"></i> to Excel
+                    </a>
+                </li>
+                <li class="list-inline-item">
+                    <a href="{{ route('employees.exportPdf') }}" class="btn btn-outline-danger">
+                        <i class="bi bi-download me-1"></i> to PDF
+                    </a>
+                </li>
+                <li class="list-inline-item">|</li>
+                <li class="list-inline-item">
+                    <a href="{{ route('employees.create') }}" class="btn btn-primary">
+                        <i class="bi bi-plus-circle me-1"></i> Create Employee
+                    </a>
+                </li>
+            </ul>
+        </div>
+</div>
+
+    <hr>
+    <div class="table-responsive border p-3 rounded-3">
+        <table class="table table-bordered table-hover table-striped mb-0 bg-white datatable" id="employeeTable">
+            <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>No.</th>
+                    <th>First Name</th>
+                    <th>Last Name</th>
+                    <th>Email</th>
+                    <th>Age</th>
+                    <th>Position</th>
+                    <th></th>
+                </tr>
+            </thead>
+
+            {{-- <tbody>
+                @foreach ($employees as $employee)
+                    <tr>
+                        <td>{{ $employee->firstname }}</td>
+                        <td>{{ $employee->lastname }}</td>
+                        <td>{{ $employee->email }}</td>
+                        <td>{{ $employee->age }}</td>
+                        <td>{{ $employee->position->name }}</td>
+                        <td>
+                            <div class="d-flex">
+                                <a href="{{ route('employees.show', ['employee' => $employee->id]) }}" class="btn btn-outline-dark btn-sm me-2"><i class="bi-person-lines-fill"></i></a>
+                                <a href="{{ route('employees.edit', ['employee' => $employee->id]) }}" class="btn btn-outline-dark btn-sm me-2"><i class="bi-pencil-square"></i></a>
+
+                                <div>
+                                    <form action="{{ route('employees.destroy', ['employee' => $employee->id]) }}" method="POST">
+                                        @csrf
+                                        @method('delete')
+                                        <button type="submit" class="btn btn-outline-dark btn-sm me-2"><i class="bi-trash"></i></button>
+                                    </form>
+                                </div>
+
+                            </div>
+                        </td>
+                    </tr>
+                @endforeach
+            </tbody> --}}
+
+        </table>
+    </div>
+</div>
+@endsection
